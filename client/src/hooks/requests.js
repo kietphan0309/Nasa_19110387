50f5ef0a9fbd89e580b9cdad9f18e0ea09f5ab62
kietphan0309 <dev101.fostech@gmail.com>
2022-11-03 22:52:19 +0700

    create project

---
+++ client/src/hooks/requests.js
@@ -0,0 +1,25 @@
+const API_URL = "http://localhost:8000";
+
+async function httpGetPlanets() {
+  const res = await fetch(`${API_URL}/planets`);
+  return await res.json();
+}
+
+async function httpGetLaunches() {
+  const res = await fetch(`${API_URL}/launches`);
+  return await res.json().sort((a, b) => {
+    return a.flightNumber - b.flightNumber;
+  });
+}
+
+async function httpSubmitLaunch(launch) {
+  // TODO: Once API is ready.
+  // Submit given launch data to launch system.
+}
+
+async function httpAbortLaunch(id) {
+  // TODO: Once API is ready.
+  // Delete launch with given ID.
+}
+
+export { httpGetPlanets, httpGetLaunches, httpSubmitLaunch, httpAbortLaunch };
